793e8a3a3498c7571f4253bb528c8861336a1bf6
Silverfeelin <Silverfeelin@gmail.com>
2016-05-24 18:11:10 +0200

    Add files via upload

--- SpawnableItemPack/scripts/spawnableItemPack.lua
+++ SpawnableItemPack/scripts/spawnableItemPack.lua
@@ -4,6 +4,8 @@ sip = spawnableItemPack
 function sip.init()
   mui.setTitle("^shadow;Spawnable Item Pack", "^shadow;Spawn anything, for free!")
   mui.setIcon("/interface/sip/icon.png")
+  sip.items = {}
+  sip.categories = {}
 end
 
 function sip.update(dt)
@@ -14,6 +16,10 @@ function sip.uninit()
 
 end
 
-function sip.test()
-  mui.setTitle("Test case success!")
+function sip.categorySelected()
+
+end
+
+function sip.search()
+
 end
